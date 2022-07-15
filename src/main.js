cc541f861e315c4288abfe05eb2b82bf582f65c4
farhan-helmy <farhanhlmy@gmail.com>
2022-07-15 16:41:11 +0800

    remove autho

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { ViteSSG } from 'vite-ssg'
 import generatedRoutes from 'virtual:generated-pages'
 import { setupLayouts } from 'virtual:generated-layouts'
-import { createAuth0 } from '@auth0/auth0-vue';
 import { createPinia } from 'pinia';
 import Datepicker from '@vuepic/vue-datepicker';
 import '@vuepic/vue-datepicker/dist/main.css'
@@ -11,11 +10,6 @@ import './styles/index.css'
 
 
 const routes = setupLayouts(generatedRoutes)
-const auth0 = createAuth0({
-  domain: "dev-t4u8li5g.us.auth0.com",
-  client_id: "MO1rkiTWTiNNSG1k4ly3t9KSx5AOfaR2",
-  redirect_uri: 'http://localhost:3000/home'
-})
 
 // https://github.com/antfu/vite-ssg
 export const createApp = ViteSSG(
@@ -23,7 +17,7 @@ export const createApp = ViteSSG(
     { routes, base: import.meta.env.BASE_URL },
     ({ app }) => {
       // install plugins etc.
-      app.use(auth0)
+      
       app.use(createPinia())
       app.component('Datepicker', Datepicker);
     },
